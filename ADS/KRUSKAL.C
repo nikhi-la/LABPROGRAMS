448e8b7fe4175356d079b9cb9683a090f9103d60
nikhi-la <128206204+nikhi-la@users.noreply.github.com>
2023-11-23 20:24:45 +0530

    Add files via upload

--- ADS/KRUSKAL.C
+++ ADS/KRUSKAL.C
@@ -7,7 +7,7 @@ int parent[10];
 
 void main()
 {
-    int n,i,j,k,mincost,x,costs,u,v,m=0,temp;
+    int n,i,j,k,mincost,x,costs,u,v,m,temp;
 	int cost[10][10],t[10][2],heap[10],U[10],V[10];
 
 	clrscr();
@@ -40,9 +40,10 @@ void main()
 				}
 				else
 				{
-					heap[x]=cost[i][j];
-					U[x]=i;
-					V[x]=j;
+							     //if edge is (1,2) and cost[1][2]=3
+					heap[x]=cost[i][j];  //store cost of edge (ie 3) in an array heap
+					U[x]=i;              //store vertex (ie 1) in an array U
+					V[x]=j;              //store vertex (ie 2) in an array V
 					x++;
 				}
 				cost[j][i]=cost[i][j];
@@ -60,16 +61,21 @@ void main()
 		}
 		printf("\n");
 	}
+	//initialize parent of each node to -1
 	for(i=1;i<=n;++i)
 	{
 		parent[i]=-1;
 	}
-	/*for(i=1;i<=n;++i)
+	/* print parent of each node
+
+	for(i=1;i<=n;++i)
 	{
 		printf("%d ",parent[i]);
 	}
 	printf("\n"); */
 
+
+	//sort costs in heap array
 	for(i=0;i<x;++i){
 		for(j=i+1;j<x;++j){
 			if(heap[i]>heap[j]){
@@ -77,10 +83,12 @@ void main()
 				heap[i]=heap[j];
 				heap[j]=temp;
 
+				//swap array U with respect to heap
 				temp=U[i];
 				U[i]=U[j];
 				U[j]=temp;
 
+				//swap array V with respect to heap
 				temp=V[i];
 				V[i]=V[j];
 				V[j]=temp;
@@ -92,22 +100,23 @@ void main()
 
 		       printf("%d<- %d-> %d\n",U[i],heap[i],V[i]);
 	}
+	m=0;  //m is the index for spanning tree
 	mincost=0;
 	while(m<n-1)
 	{
 	for(i=0;i<x;++i)
 	{
-	       heap[i]=0;
-		u=U[i];
-		v=V[i];
+	       heap[i]=0;   //delete edge with minimum cost(make it null)
+		u=U[i];     //corresponding vertex of minimum cost edge
+		v=V[i];     //corresponding vertex of minimum cost edge
 		j=find(u);
 		k=find(v);
 		//printf("%d %d\n",j,k);
-		if(j!=k)
+		if(j!=k)    //j and k on different set
 		{
 		   m=m+1;
-		   t[m][1]=u;
-		   t[m][2]=v;
+		   t[m][1]=u; //add vertex to spanning tree
+		   t[m][2]=v; //add vertex to spanning tree
 		   mincost=mincost+cost[u][v];
 		   //printf("\n%d \n",mincost);
 		   unionjk(u,v);
@@ -133,7 +142,7 @@ void main()
 
 int find(u)
 {
-	if(parent[u]==-1)
+	if(parent[u]==-1)  //single node
 	{
 		parent[u]=u;
 	}
@@ -143,5 +152,5 @@ int find(u)
 
 void unionjk(int u,int v)
 {
-	parent[v]=parent[u];
+	parent[v]=parent[u]; //make same parent for u and v
 }
