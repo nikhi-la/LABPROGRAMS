67555686ebd7e5151b05cf437ad3b6620e8874f9
nikhi-la <128206204+nikhi-la@users.noreply.github.com>
2023-11-29 19:13:08 +0530

    Delete Nikhila/63Program.py

--- Nikhila/63Program.py
+++
@@ -1,9 +0,0 @@
-#AssertError for negative input
-
-try:
-    n=int(input("Enter number\n"))
-    assert(n>0),'Negative Input'
-    print('Number is',n)
-    
-except AssertionError as ae:
-    print(ae)
